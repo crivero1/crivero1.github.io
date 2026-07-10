2fca52c45ed6f5e188496f1262a78b8d8a29d5a6
amyaranta <afsalas@uc.cl>
2026-07-10 08:47:22 +0200

    small changes

--- src/components/boxPapers.jsx
+++ src/components/boxPapers.jsx
@@ -3,18 +3,25 @@ function listToP(listToParse){
     return (
         <>
         {listToParse.map((line, index) => (
-            <div key={index} className="text-left text-body my-4">
-                {/* <div className="text-left text-body my-2"> */}
+            <div key={index} className="text-left text-body my-1">
+                {/* <div className="pb-2 text-2xl text-left font-medium text-gray-600">
+                    {line["title"]}
+                </div> */}
+                <div className="text-left text-body my-2">
                     <div className="flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 10" fill="currentColor" className="size-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 10" fill="currentColor" className="size-4">
                         <path fillRule="evenodd" d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z" clipRule="evenodd" />
                         </svg>
-                        {/* {line["authors"]} */}
-                    {/* </div> */}
-                {/* </div> */}
-                {/* <div className="text-left text-body my-2"> */}
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                        <path d="M160 96L480 96C515.3 96 544 124.7 544 160L544 480C544 515.3 515.3 544 480 544L160 544C124.7 544 96 515.3 96 480L96 160C96 124.7 124.7 96 160 96z"/>
+                        </svg> */}
+                        {/* <SquareIcon></SquareIcon> */}
+                        {line["authors"]}
+                    </div>
+                </div>
+                <div className="text-left text-body my-2">
                     <p className="pl-4">
-                        <span className="italic">{line["title"]}</span>. Together with {line["authors"]}. In {line["type"]} {line["year"]}
+                        <span className="italic">{line["title"]}</span>. {line["type"]}, {line["year"]}
                     </p>
                 </div>
             </div>
@@ -25,15 +32,11 @@ function listToP(listToParse){
 
 export default function BoxResearch( {papers, color, border_shape, bold_color, title} ) {
     // console.log(papers)
-    
     return (
         <div className={`m-2 flex flex-col p-5 content-around justify-start content-between ${color} ${border_shape}`}>
             <div className="pb-2 text-2xl text-left font-medium text-gray-600">
                 {title}
             </div>
-            {/* <svg height="50" width="300" xmlns="http://www.w3.org/2000/svg">
-                <line x1="0" y1="10" x2="250" y2="10" style={{stroke:black, stroke-width:3}} />
-            </svg> */}
             <div className={`text-justify mt-2 flex-1 ${bold_color}`}>
                 {listToP(papers)}
             </div>
